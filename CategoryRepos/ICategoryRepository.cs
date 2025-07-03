f1ca9639c283471f7f38756f72bce23e82fadfbf
Berkant Gunes <gunesberkant96@gmail.com>
2025-07-03 17:34:52 +0300

    Add files via upload

---
+++ CategoryRepos/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Project5_DapperNorthwind.DTOs.CategoryDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project5_DapperNorthwind.Repos.CategoryRepos
+{
+    public interface ICategoryRepository
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategoryAsync(int id); // id ye göre silme işlemi yapacağız.
+        Task<GetByIdCategoryDto> GetByIdCategoryAsync(int id); // burada da adı üstünde id sine göre işlem yaparız.
+    }
+}
